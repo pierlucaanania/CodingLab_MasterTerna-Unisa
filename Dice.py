857632b03364d3981e3af1545a9327b4e7193055
pierluca <p.l.anania@gmail.com>
2024-12-04 14:05:51 +0100

    First Lesson Python

---
+++ Dice.py
@@ -0,0 +1,24 @@
+import random
+
+upper = int(input("Insert higher number: "))
+lower = int(input("Insert lower number: "))
+
+iteraction = 0
+done = False
+
+if upper > lower:
+    choice = random.randint(lower,upper)
+    while not done:
+        number = int(input("Guess the number: "))
+        iteraction += 1
+        if number < choice:
+            print("Your number is lower")
+        elif number > choice:
+            print("Your number is higher")
+        elif number == choice:
+            print("Number guessed")
+            done = True
+    print(f"Finished, the number was {choice} ")
+    print(f"Iteraction: {iteraction}")
+else:
+    print("Wrong values")
